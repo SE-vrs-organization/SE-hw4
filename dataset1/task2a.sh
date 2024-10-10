5b5e5c72d9499fa33d3070000f70b563b72e3f42
Shubham <shubham.kakirde@spit.ac.in>
2024-10-10 19:28:09 -0400

    task 2 fix

--- dataset1/task2a.sh
+++ dataset1/task2a.sh
@@ -1,6 +1,7 @@
-grep -o -E "CSC510|sample" file* \
+grep -o -E "CSC510" file* \
 | uniq -c \
 | grep -v -E "^\s*[012].*:CSC510$" \
 | sed -E "s/\s+[0-9]*\s//g" \
-| sed -E "s/:(CSC510|sample)$//g" \
-| uniq
+| sed -E "s/:(CSC510)$//g" \
+| xargs grep -c "sample" \
+| sed -E "s/:[0-9]+$//g" \
